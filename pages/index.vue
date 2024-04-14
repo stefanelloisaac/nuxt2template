5cd8f7567076eb4abf37b3ecc2c151a57d229b27
Isaac <i.stefanello@unochapeco.edu.br>
2024-04-14 14:21:24 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="start">
+        Template Nuxt2
+        <footer>
+          <small>
+            <em>stefanelloisaac</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
